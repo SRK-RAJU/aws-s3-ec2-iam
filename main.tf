210cd34719b0a88f3b1b1c9b404daf7369ed77b1
91994 <suddala.rajukumar123@gmail.com>
2022-10-04 17:28:50 +0530

    updating

--- main.tf
+++ main.tf
@@ -4,6 +4,33 @@ provider "aws" {
 resource "aws_s3_bucket" "blog" {
   bucket = var.bucket_name
   acl="private"
+  lifecycle_rule{
+    id= "archive"
+    enabled =true
+    transition {
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days=60
+      storage_class = "GLACIER"
+    }
+
+  }
+  versioning {
+    enabled = true
+  }
+  tags = {
+    Environment:"QA"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_object" "object1" {
@@ -14,9 +41,14 @@ resource "aws_s3_bucket_object" "object1" {
   etag=filemd5("html/${each.value}")
   content_type = "text/html"
 }
-resource "aws_s3_bucket_versioning" "s3-version" {
-  bucket = aws_s3_bucket.blog.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+#resource "aws_s3_bucket_versioning" "s3-version" {
+#  bucket = aws_s3_bucket.blog.id
+#  versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
+
+resource "aws_s3_bucket_metric" "enable-metrics-block" {
+  bucket = var.bucket_name
+  name   = "EntireBucket"
 }
