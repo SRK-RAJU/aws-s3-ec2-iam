284b9bc974b623723fa8f1ca5138d2e4e0ff3285
91994 <suddala.rajukumar123@gmail.com>
2022-10-04 18:04:17 +0530

    updating

--- iam.tf
+++ iam.tf
@@ -38,7 +38,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 
 resource "databricks_aws_s3_mount" "this" {
   instance_profile=aws_iam_instance_profile.ec2_profile.id
-  instance_profile = aws_instance.web-pub.id
+#  instance_profile = aws_instance.web-pub.id
   s3_bucket_name = aws_s3_bucket.blog.bucket
 mount_name = "experiments"
 }
