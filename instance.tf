f08cfec83faf72081d528d4bf29903631e8320dd
91994 <suddala.rajukumar123@gmail.com>
2022-10-04 18:40:03 +0530

    updating

--- instance.tf
+++ instance.tf
@@ -1,7 +1,25 @@
+resource "tls_private_key" "dev_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.generated_key_name
+  public_key = tls_private_key.dev_key.public_key_openssh
+
+  provisioner "local-exec" {    # Generate "terraform-key-pair.pem" in current directory
+    command = <<-EOT
+      echo '${tls_private_key.dev_key.private_key_pem}' > ./'${var.generated_key_name}'.pem
+      chmod 400 ./'${var.generated_key_name}'.pem
+    EOT
+  }
+
+}
+
 resource "aws_instance" "web-pub" {
   ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
-#  key_name = var.generated_key_name
+  key_name = var.generated_key_name
 #  security_groups = [ aws_security_group.allow-sg-pub.id ]
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 #  subnet_id = aws_subnet.public-sub.id
